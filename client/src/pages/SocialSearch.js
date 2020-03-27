194c5ea6c6227b4921417c65472d82dfc5ff0c4a
cameronmoodley <moodley.cameron@gmail.com>
2020-03-27 16:21:44 +0100

    fixed jonmars work

--- client/src/pages/SocialSearch.js
+++ client/src/pages/SocialSearch.js
@@ -1,65 +1,58 @@
 import React, { useState, useEffect } from "react";
 
-import { BASE_URL, USERS } from "../constants/constants";
+import { BASE_URL, USERS, IMG_URL } from "../constants/constants";
 import axios from "axios";
 import FollowFriendCard from "../components/FollowFriendCard";
 
-function SocialSearch(props) {
-  const { name, image, score } = props;
+function SocialSearch() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
 
-  function SocialSearch() {
-    const [users, setUsers] = useState([]);
-    const [filteredUsers, setFilteredUsers] = useState([]);
+  useEffect(() => {
+    axios.get(BASE_URL + USERS).then(results => {
+      console.log(results.data);
+      setUsers(results.data);
+    });
+  }, []);
 
-    useEffect(() => {
-      axios.get(BASE_URL + USERS).then(results => {
-        console.log(results.data);
-        setUsers(results.data);
-      });
-    }, []);
+  const handleChange = input => {
+    let filteredArray = users.filter((value, index) => {
+      return value.username
+        .toLowerCase()
+        .includes(input.target.value.toLowerCase());
+    });
+    setFilteredUsers(filteredArray);
+  };
 
-    const handleChange = input => {
-      let filteredArray = users.filter((value, index) => {
-        return value.username
-          .toLowerCase()
-          .includes(input.target.value.toLowerCase());
-      });
-      setFilteredUsers(filteredArray);
-    };
-    return (
-      <div className="App">
-        <h1>Search For a Friend</h1>
-        <form>
-          <input
-            type="text"
-            name="search"
-            className=""
-            onChange={handleChange}
-          />
-        </form>
-        <div>
-          {filteredUsers.length > 0 ? (
-            filteredUsers.map((value, key) => {
-              return (
-                <div key={key}>
-                  {value.username}, {value.points}
-                </div>
-              );
-            })
-          ) : (
-            <div>There are no users with the name searched</div>
-          )}
-        </div>
+  const handleClick = () => {
+    console.log("Jonmar sucks");
+  };
+
+  return (
+    <div className="App">
+      <h1>Search For a Friend</h1>
+      <form>
+        <input type="text" name="search" className="" onChange={handleChange} />
+      </form>
+      <div>
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((value, key) => {
+            return (
+              <FollowFriendCard
+                key={key}
+                name={value.username}
+                image={IMG_URL + value.profileimage[0].url}
+                score={value.points}
+                onClick={handleClick}
+              />
+            );
+          })
+        ) : (
+          <div>There are no users with the name searched</div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-FollowFriendCard.defaultProps = {
-  name: "user",
-  image:
-    "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2776&q=80",
-  score: "2 354"
-};
 
 export default SocialSearch;
