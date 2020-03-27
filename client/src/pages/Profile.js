e9fd410a669324cb8bfa7e8aa236ff9d914c3ca1
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-03-27 20:54:31 +0100

    wip: profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect} from 'react'
+import React from 'react'
+import { IMG_URL } from "../constants/constants";
+import Button from "../components/Button";
 
 function Profile() {
-  console.log("Profile init");
+  const user = JSON.parse(sessionStorage.getItem("User"));
+  const [profileImage] = user.profileimage;
+  const profileUrl = `${IMG_URL}${profileImage.url}`;
   return(
     <div>
       <h1>Profile</h1>
-      <figure><img src="" alt=""/></figure>
+      <figure>
+        <img src={profileUrl} alt={user.Firstname} />
+      </figure>
+      <div>{user.Firstname} {user.Lastname}</div>
+      <Button primary >Edit Profile</Button>
+      <div>
+        <h2>Tasks Completed</h2>
+      </div>
     </div>
   )
 }
