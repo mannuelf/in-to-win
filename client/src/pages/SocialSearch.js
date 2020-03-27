e12e3e0814942111adbf34f05c4b8bb2ac756399
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-03-27 20:32:37 +0100

    cahnge the name of handleClick to handleFollow

--- client/src/pages/SocialSearch.js
+++ client/src/pages/SocialSearch.js
@@ -28,7 +28,7 @@ function SocialSearch() {
     setFilteredUsers(filteredArray);
   };
 
-  const handleClick = friendid => {
+  const handleFollow = friendid => {
     let user = JSON.parse(sessionStorage.getItem("User"));
     axios
       .post(
@@ -64,7 +64,7 @@ function SocialSearch() {
                 username={value.username}
                 image={img}
                 score={value.points}
-                onClick={handleClick}
+                onClick={handleFollow}
               />
             );
           })
