24b02586ef383d5087281125c98d2dcd6958ecb6
cameronmoodley <moodley.cameron@gmail.com>
2020-03-27 14:58:58 +0100

    added in search logic

--- client/src/pages/SocialSearch.js
+++ client/src/pages/SocialSearch.js
@@ -4,18 +4,43 @@ import { BASE_URL, USERS } from "../constants/constants";
 import axios from "axios";
 
 function SocialSearch() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
   useEffect(() => {
     axios.get(BASE_URL + USERS).then(results => {
-      console.log(results);
+      console.log(results.data);
+      setUsers(results.data);
     });
   }, []);
-  const handleChange = input => {};
+
+  const handleChange = input => {
+    let filteredArray = users.filter((value, index) => {
+      return value.username
+        .toLowerCase()
+        .includes(input.target.value.toLowerCase());
+    });
+    setFilteredUsers(filteredArray);
+  };
   return (
     <div className="App">
       <h1>Search For a Friend</h1>
       <form>
         <input type="text" name="search" className="" onChange={handleChange} />
       </form>
+      <div>
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((value, key) => {
+            return (
+              <div key={key}>
+                {value.username}, {value.points}
+              </div>
+            );
+          })
+        ) : (
+          <div>There are no users with the name searched</div>
+        )}
+      </div>
     </div>
   );
 }
