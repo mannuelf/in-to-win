aeaf5ef37d14d55c03f08958be2067dad137e284
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-03-27 15:52:13 +0100

    added follow-friend-card

--- client/src/components/FollowFriendCard.jsx
+++ client/src/components/FollowFriendCard.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import FollowButton from "./common/FollowButton";
 
-function FollowFriendCard({ onClick }) {
+function FollowFriendCard({ image, name, score, onClick }) {
   return (
-    <div className="follow-friend-card">
-      <img src="" alt="" />
+    <div className="follow-friend-card" style={style_FFC}>
+      <img src={image} alt={name} style={style_IMG} />
       <div className="score">
-        Score: <span className="yellow-dark-text-">2 439</span>
+        Score: <span className="yellow-dark-text-">{score}</span>
       </div>
-      <button className="btn yellow-text" onClick={onClick}>
-        Follow
-      </button>
+      <FollowButton onClick={onClick} />
     </div>
   );
 }
+const style_FFC = {
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "16px",
+  backgroundColor: "black",
+  color: "white"
+};
+
+const style_IMG = {
+  width: "32px",
+  height: "32px",
+  backgroundSize: "100%",
+  borderRadius: "50%"
+};
 
 export default FollowFriendCard;
--- client/src/pages/SocialSearch.js
+++ client/src/pages/SocialSearch.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 
 import { BASE_URL, USERS } from "../constants/constants";
 import axios from "axios";
+import FollowFriendCard from "../components/FollowFriendCard";
 
-function SocialSearch() {
+function SocialSearch(props) {
+  const { name, image, score } = props;
+
   useEffect(() => {
     axios.get(BASE_URL + USERS).then(results => {
       console.log(results);
@@ -22,8 +25,23 @@ function SocialSearch() {
       <form>
         <input type="text" name="search" className="" onChange={handleChange} />
       </form>
-      <div className="card-deck"></div>
+      <div className="card-deck">
+        <FollowFriendCard
+          image={image}
+          score={score}
+          name={[name]}
+          onClick={handleFollow}
+        />
+      </div>
     </div>
   );
 }
+
+FollowFriendCard.defaultProps = {
+  name: "user",
+  image:
+    "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2776&q=80",
+  score: "2 354"
+};
+
 export default SocialSearch;
