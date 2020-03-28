2956bc3e3d1a5b87f3fb4d3519f312b9ede0d491
cameronmoodley <moodley.cameron@gmail.com>
2020-03-28 19:11:17 +0100

    added profile card to general layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 import Login from "./pages/Login";
 
 import { Link } from "react-router-dom";
+import { IMG_URL } from "./constants/constants";
 
 import styled from "styled-components";
 import Navbar from "./components/Navbar";
 import Toast from "./components/Toast";
+import HeaderCard from "././components/HeaderCard";
 const ErrorComp = styled.p``;
 
 const LoginCont = styled.div`
@@ -24,6 +26,18 @@ function App({ children }) {
   const [isUserLoggedIn, setisUserLoggedIn] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
   const [showDialog, setShowDialog] = useState(false);
+
+  const user = JSON.parse(sessionStorage.getItem("User"));
+  const [url] = user.profileimage;
+  const username = user.username;
+  const firstName = user.Firstname;
+
+  const points = user.points;
+  const imgUrl = user.profileimage.length ? `${IMG_URL}${url.url}` : false;
+  const noImgUrl =
+    "https://i7.pngguru.com/preview/117/435/819/user-silhouette-my-account-icon.jpg";
+  const profileImgUrl = user.profileimage.length ? imgUrl : noImgUrl;
+
   const handleLogin = () => {
     setisUserLoggedIn(true);
   };
@@ -39,13 +53,20 @@ function App({ children }) {
     <div className="App">
       {isUserLoggedIn && sessionStorage.getItem("JWT") !== null ? (
         <div>
-          <Link to="/">Home</Link>
+          <HeaderCard
+            url={profileImgUrl}
+            username={username}
+            firstName={firstName}
+            profileImgUrl={profileImgUrl}
+            points={points}
+          />
+          {/* <Link to="/">Home</Link>
           <Link to="/affiliates">affiliates</Link>
           <Link to="/ask-corona-go">Get your facts straight</Link>
           <Link to="/search-for-a-friend">Find a Friend</Link>
           <Link to="/leaderboard">Leaderboard</Link>
           <Link to="/profile">Profile</Link>
-          <Link to="/fileupload">Test Link</Link>
+          <Link to="/fileupload">Test Link</Link> */}
           <button onClick={handleLogout}>Logout</button>
           {children}
           <Toast
