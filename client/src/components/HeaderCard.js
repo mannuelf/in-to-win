dc8de9da4c8914bae55a01113ebea6c9c9c10674
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-03-28 15:32:43 +0100

    header content

--- client/src/components/HeaderCard.js
+++ client/src/components/HeaderCard.js
@@ -4,31 +4,40 @@ import theme from "../GlobalStyle/Theme";
 
 const HeaderCardContainer = styled.div`
   background-color: ${theme.gradient};
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
 `;
 
 const HeaderCardImage = styled.div`
   width: 100%;
 `;
 
+const HeaderCardMedia = styled.div`
+  flex-grow: 1;
+`;
+
+const HeaderCardContent = styled.div`
+ flex-grow: 2;
+`;
+
 const coverImg = {
-  width: "100%"
+  width: "64px",
+  height: "64px",
+  "border-radius": "50%"
 };
 
 function HeaderCard({username, firstName, profileImgUrl, points}) {
   return(
     <HeaderCardContainer>
-      <div className="header-card-media">
-        <div className="header-card-media-left">
+        <HeaderCardMedia>
           <HeaderCardImage />
-          <figure className="header-card-image">
-            <img style={coverImg} src={profileImgUrl} alt={firstName} />
-          </figure>
-        </div>
-        <div className="header-card-media-content">
+          <img style={coverImg} src={profileImgUrl} alt={firstName} />
+        </HeaderCardMedia>
+        <HeaderCardContent>
           <p className="header-card-content-title">@{username}</p>
           <p className="header-card-content-subtitle">{points}</p>
-        </div>
-      </div>
+        </HeaderCardContent>
     </HeaderCardContainer>
   )
 }
