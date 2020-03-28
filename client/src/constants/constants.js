fe637a74f03d6bfb890132923d7359e666d7aa08
Kingsley <k@n.com>
2020-03-28 10:08:39 +0100

    removed corsanywhere as cors is now enabled on the server

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  "https://cors-anywhere.herokuapp.com/http://34.243.84.61/";
+export const BASE_URL = "http://34.243.84.61/";
 export const AUTH_URL = "auth/local";
 export const AFFILIATES_URL = "affliates";
 export const CORONA_FACTS = "coronafacts";
