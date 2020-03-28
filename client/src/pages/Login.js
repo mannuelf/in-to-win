7f3d42c0655c33c35cefa5d55dd2c9d06c8c7dbb
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-03-28 20:20:11 +0100

    fixed login design

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,18 +7,6 @@ import theme from "../GlobalStyle/Theme";
 
 import { BASE_URL, AUTH_URL } from "../constants/constants";
 
-const Form = styled.form`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  border: 5px 5px 0 0;
-  background-color: ${theme.colors.grey};
-  width: 100%;
-  max-height: 800px;
-`;
-
-const ColouredSpan = styled.span``;
 function Login({ updateErrors, updateLoginStatus }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -49,23 +37,29 @@ function Login({ updateErrors, updateLoginStatus }) {
 
   return (
     <div>
-      <h1>InToWin</h1>
-      <span>The more you in, the more you win.</span>
-      <div>
+      <div className="logo-container" style={style_logoContainer}>
+        <h1>InToWin</h1>
+        <span>The more you in, the more you win.</span>
+      </div>
+      <div style={style_loginForm}>
+        <h2 style={style_heading}>Login</h2>
         <Form onSubmit={handleSubmit}>
-          <h2>Username</h2>
+          {" "}
           <input
             type="text"
             name="username"
             onChange={handleChange}
             className=""
+            placeholder="Username"
+            style={style_loginInput}
           />
-          <h2>Password</h2>
           <input
             type="password"
             name="password"
             onChange={handleChange}
             className=""
+            placeholder="Password"
+            style={style_loginInput}
           />
           <Button
             as="input"
@@ -80,5 +74,54 @@ function Login({ updateErrors, updateLoginStatus }) {
     </div>
   );
 }
+
+const style_loginForm = {
+  width: "100%",
+  marginTop: "64px",
+  height: "70vh",
+  padding: "16px",
+  boxSizing: " border-box",
+  backgroundColor: `${theme.colors.grey}`,
+  position: "absolute",
+  bottom: "0",
+  left: "0",
+  borderTopLeftRadius: "20px",
+  borderTopRightRadius: "20px"
+};
+
+const style_logoContainer = {
+  width: "100%",
+  paddingTop: "64px",
+  height: "40vh",
+  position: "absolute",
+  top: "0",
+  left: "0"
+};
+
+const style_heading = {
+  width: "100%",
+  textAlign: "center",
+  fontSize: "16px",
+  fontWeight: "400",
+  margin: "0 0 64px 0"
+};
+
+const style_loginInput = {
+  border: "none",
+  borderBottom: `2pt solid ${theme.colors.primary}`,
+  backgroundColor: "transparent",
+  width: "100%",
+  padding: "8px 0",
+  fontSize: "24px",
+  color: `${theme.colors.text}`,
+  marginBottom: "16px",
+  textAlign: "center"
+};
+
+const Form = styled.form`
+  border: 5px 5px 0 0;
+  width: 100%;
+  max-height: 800px;
+`;
 
 export default Login;
