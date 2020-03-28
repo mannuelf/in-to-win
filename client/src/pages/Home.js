7945c86c883248abed17c14643ac227fb6966314
Kingsley <k@n.com>
2020-03-28 23:07:25 +0100

    output list of tasks on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Task as TaskAPI,
+  User as UserAPI 
+} from '../api/api';
+import TaskCard from "../components/TaskCard";
 
 function Home() {
+
+  const user = JSON.parse(sessionStorage.getItem("User"));
+
+  const [state, setState] = useState({
+    tasks: []
+  });
+  useEffect(() => {
+    TaskAPI.getAll(user.id)
+      .then(allTasks => {
+        setState({
+          ...state,
+          tasks: allTasks,
+        })
+      });
+  }, [user.id]);
+
   return (
     <div className="App">
       <h1>Home Page</h1>
+
+      <ul>
+        {state.tasks.map(task => (
+          <li key={task.id}>
+            <TaskCard title={task.name} description={task.description} difficulty={task.difficulty} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
