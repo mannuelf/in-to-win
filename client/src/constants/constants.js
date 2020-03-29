48c9a5979dd84a64b5f39c5fca911ab0a4cc17fa
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-03-29 12:53:31 +0200

    update to http

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "http://34.243.84.61/";
+export const BASE_URL = "https://www.sanify.xyz/";
 export const AUTH_URL = "auth/local";
 export const AFFILIATES_URL = "affliates";
 export const CORONA_FACTS = "coronafacts";
-export const IMG_URL = "http://34.243.84.61/";
+export const IMG_URL = "https://www.sanify.xyz/";
 export const USERS = "users";
 export const CUSTOMER_FRIENDS = "customerfriends";
 export const TASK_IMAGES = "taskimages";
